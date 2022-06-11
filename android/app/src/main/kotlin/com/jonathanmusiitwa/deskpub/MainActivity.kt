33854b59e802d55ded5cc8f840cbc68024bf36d0
TJMusiitwa <jonamusiitwa@outlook.com>
2022-06-11 11:28:33 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/jonathanmusiitwa/deskpub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jonathanmusiitwa.deskpub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import macos_ui
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
