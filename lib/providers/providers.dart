91290b05e091e144e5a398ec40680e682224b95f
TJMusiitwa <jonamusiitwa@outlook.com>
2024-03-02 04:19:32 +0300

    Add new providers for package information and search results

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:deskpub/models/trending.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github/github.dart';
@@ -32,23 +34,11 @@ final googlePackagesProvider =
 
 final todayTrendingProvider =
     FutureProvider.autoDispose<List<Trending>>((ref) async {
-  var request = http.Request(
-      'GET', Uri.parse('http://api.flutter.space/v1/trending/today.json'));
-
-  //http.StreamedResponse response = await request.send();
-
   http.Response response = await http
       .get(Uri.parse('http://api.flutter.space/v1/trending/today.json'));
-
-  // if (response.statusCode == 200) {
-  //   print(await response.stream.bytesToString());
-  // } else {
-  //   print(response.reasonPhrase);
-  // }
   ref.keepAlive();
 
   final trending = trendingFromJson(response.body);
-
   return trending;
 });
 
@@ -68,4 +58,78 @@ final monthTrendingProvider = FutureProvider<List<Trending>>((ref) async {
 
   final trending = trendingFromJson(response.body);
   return trending;
+});
+
+final singlePackageProvider = FutureProvider.autoDispose
+    .family<PubPackage, String>((ref, packageName) async {
+  final client = ref.watch(pubClientProvider);
+  //ref.onDispose(() => client.close());
+  return await client.packageInfo(packageName);
+});
+
+final scorePackageProvider = FutureProvider.autoDispose
+    .family<PackageScore, String>((ref, packageName) async {
+  final client = ref.watch(pubClientProvider);
+  //ref.onDispose(() => client.close());
+  return await client.packageScore(packageName);
+});
+
+final metricsPackageProvider = FutureProvider.autoDispose
+    .family<PackageMetrics?, String>((ref, packageName) async {
+  final client = ref.watch(pubClientProvider);
+  //ref.onDispose(() => client.close());
+  return await client.packageMetrics(packageName);
+});
+
+final packageReadmeProvider =
+    FutureProvider.autoDispose.family<String, String>((ref, packageName) async {
+  final client = ref.watch(pubClientProvider);
+  final githubClient = ref.watch(githubClientProvider);
+  final packageInfo = client.packageInfo(packageName);
+  final repositoryUrl = await packageInfo.then((value) {
+    if (value.latestPubspec.unParsedYaml!['repository'] != null) {
+      return value.latestPubspec.unParsedYaml!['repository'].toString();
+    } else if (value.latestPubspec.homepage != null) {
+      return value.latestPubspec.homepage!;
+    } else {
+      return 'Nothing here';
+    }
+  });
+
+  final readme = githubClient.repositories.getReadme(
+      RepositorySlug(repositoryUrl.split('/')[3], repositoryUrl.split('/')[4]));
+
+  log('Repository URL: $repositoryUrl');
+
+  //log(RepositorySlug('flutterfire', repositoryUrl.split('/').last));
+
+  log(RepositorySlug(repositoryUrl.split('/')[3], repositoryUrl.split('/')[4])
+      .toString());
+
+  ref.keepAlive();
+
+  return readme.then((value) => value.text);
+});
+
+final packagePublisherProvider = FutureProvider.autoDispose
+    .family<PackagePublisher, String>((ref, packageName) async {
+  return await ref.watch(pubClientProvider).packagePublisher(packageName);
+});
+
+final searchPackagesProvider = FutureProvider.autoDispose.family<
+    List<PackageResult>,
+    ({
+      String searchQuery,
+      int? page,
+      SearchOrder? order,
+    })>((ref, searchOptions) async {
+  final client = ref.watch(pubClientProvider);
+
+  final searchResults = await client.search(searchOptions.searchQuery,
+      page: searchOptions.page ?? 1,
+      sort: searchOptions.order ?? SearchOrder.top);
+
+  ref.keepAlive();
+
+  return searchResults.packages;
 });
